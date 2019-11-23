3d9c74462507c39c33c3ddeb940f877325cb1765
Alex Gavrishev <alex.gavrishev@gmail.com>
2019-11-23 11:42:12 +0200

    Add wininstaller for easier windows updates

---
+++ wininstaller.js
@@ -0,0 +1,13 @@
+const electronInstaller = require('electron-winstaller');
+
+electronInstaller.createWindowsInstaller({
+    appDirectory: './distr/win/Yandex.Music.App-win32-x64',
+    outputDirectory: './distr/win/Yandex.Music.App-win32-x64-installer',
+    exe: 'Yandex.Music.App.exe',
+    name: 'YandexMusicApp',
+    setupIcon: './media/icon/yaradio_64x64.ico'
+}).then(() => {
+    console.log('It worked!');
+}, (e) => {
+    console.log(`No dice: ${e.message}`);
+});
