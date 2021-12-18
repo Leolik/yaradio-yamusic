232b8d92cefac0666ed7e44a4868b61a47c1f8aa
Alexander Gavrishev <algavris@microsoft.com>
2021-12-18 11:15:50 +0200

    Upgrade look, fix drag, tweak scrollbar

--- src/app/window.ts
+++ src/app/window.ts
@@ -1,10 +1,8 @@
 import { BrowserWindow } from "electron";
 import * as path from "path";
 import { getIconFile, getRuntimePath } from "./media";
-import { currentPlatform } from "./platform";
 
 export function createWindow(lastWindowState: Electron.Rectangle): BrowserWindow {
-  const titleStyle = (currentPlatform.isMacOs) ? "customButtonsOnHover" : "hiddenInset";
   const mainWindow = new BrowserWindow({
     title: "Yandex.Music.App - Unofficial",
     show: false,
@@ -13,7 +11,6 @@ export function createWindow(lastWindowState: Electron.Rectangle): BrowserWindow
     height: lastWindowState.height || 700,
     width: lastWindowState.width || 848,
     icon: getIconFile("yaradio_32x32.png"),
-    titleBarStyle: titleStyle,
     movable: true,
     minHeight: 700,
     minWidth: 848,
@@ -23,7 +20,6 @@ export function createWindow(lastWindowState: Electron.Rectangle): BrowserWindow
       preload: path.join(getRuntimePath(), "js", "browser.js"),
       nodeIntegration: false,
       plugins: true,
-      enableRemoteModule: false
     }
   });
   return mainWindow;
